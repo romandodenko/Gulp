ac56a189f219807d8d92ffad77a5078cbf716648
Roman Dodenko <creed6695@mail.ru>
2022-02-08 01:27:18 +0700

    инитиал коммит

---
+++ src/js/main.js
@@ -0,0 +1,55 @@
+import * as isWebp from './modules/webp.js'
+
+isWebp.isWebp()
+
+// import * as burger from './modules/burger.js'
+
+// burger.burgerOpen()
+
+
+// import * as smoothScroll from './modules/smooth-anchor.js'
+
+// smoothScroll.smoothScroll()
+
+
+import * as dynamic from './modules/dynamic-adaptive.js'
+
+dynamic.dynamic()
+
+// import * as tabs from './modules/tabs.js'
+
+// tabs.tabs()
+
+// import * as accordion from './modules/accordion.js'
+
+// accordion.accordion()
+
+// import * as scrollAddClass from './modules/add-class-for-scroll.js'
+
+// scrollAddClass.scrollAddClass()
+
+// import * as dropdown from './modules/dropdown.js'
+
+// dropdown.dropdown()
+
+
+// import * as gsapScript from './modules/gsap.js'
+
+// gsapScript.gsapScript()
+
+
+// import * as swiper from './modules/slider.js'
+
+// swiper.swiper()
+
+// import * as fur from './modules/move-the-mouse.js'
+
+// fur.fur()
+
+// import * as validate from './modules/validate.js'
+
+// validate.validate()
+
+// import * as smooth from './modules/smooth-anchor-v2.js'
+
+// smooth.smooth()
