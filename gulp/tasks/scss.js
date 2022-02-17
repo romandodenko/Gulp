5530055eff6090d0e2d69cbe7bb07ea751c34878
Roman Dodenko <creed6695@mail.ru>
2022-02-17 21:25:34 +0700

    изменение тасков

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -8,6 +8,28 @@ import sourcemaps from 'gulp-sourcemaps';
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
+   app.gulp.src(app.path.src.scssOveral)
+  .pipe(app.plugins.plumber(
+    app.plugins.notify.onError({
+      title: 'SCSS',
+      message: 'Error: <%= error.message %>',
+    }))
+  )
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(webpcss(
+    {
+      webpClass: '.webp',
+      noWebpClass: '.no-webp',
+    }
+  ))
+  .pipe(autoprefixer({
+    grid: true,
+    overrideBrowserslist: ["last 3 versions"],
+    cascade: false
+  }))
+  .pipe(sourcemaps.write())
+  .pipe(app.gulp.dest(app.path.dev.cssOveral))
   return app.gulp.src(app.path.src.scss)
   .pipe(app.plugins.plumber(
     app.plugins.notify.onError({
@@ -34,6 +56,21 @@ export const scss = () => {
 }
 
 export const cssBuild = () => {
+  //  app.gulp.src(app.path.dev.cssOveralBuild)
+  // .pipe(sass().on('error', sass.logError))
+  // .pipe(webpcss(
+  //   {
+  //     webpClass: '.webp',
+  //     noWebpClass: '.no-webp',
+  //   }
+  // ))
+  // .pipe(autoprefixer({
+  //   grid: true,
+  //   overrideBrowserslist: ["last 3 versions"],
+  //   cascade: false
+  // }))
+  // .pipe(cleanCss())
+  // .pipe(app.gulp.dest(app.path.build.cssOveral))
   return app.gulp.src(app.path.dev.cssBuild)
   .pipe(sass().on('error', sass.logError))
   .pipe(webpcss(
